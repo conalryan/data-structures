9e8ade6c1423c1416ca6fb5da1783ff7ab5a3428
cryan <conal.ryan@amadeus.com>
2020-06-28 19:26:10 -0400

    array append

--- array/java/src/main/java/com/example/ds/DynamicArray.java
+++ array/java/src/main/java/com/example/ds/DynamicArray.java
@@ -11,11 +11,37 @@ public class DynamicArray {
         this.array = new int[this.capacity];
     }
 
+    public DynamicArray(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.array = new int[this.capacity];
+    }
+
     public int getCapacity() {
         return this.capacity;
     }
 
     public int getSize() {
         return this.size;
+    }
+
+    public int getItem(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return this.array[index];
+    }
+
+    public void append(int item) {
+        if (size == capacity) {
+            this.capacity = this.capacity * 2;
+            int[] tmp = new int[this.capacity];
+            for (int i = 0; i < size; i++) {
+                tmp[i] = array[i];
+            }
+            array = tmp;
+        }
+        array[size] = item;
+        size++;
     }
 }
--- array/java/src/test/java/com/example/ds/DynamicArrayTest.java
+++ array/java/src/test/java/com/example/ds/DynamicArrayTest.java
@@ -8,13 +8,39 @@ public class DynamicArrayTest {
 
     private DynamicArray da;
 
-    @Before
-    public void before() {
+    @Test
+    public void testEmptyConstructor() {
         this.da = new DynamicArray();
+        assertEquals(this.da.getCapacity(), 10);
+        assertEquals(this.da.getSize(), 0);
     }
 
     @Test
     public void testConstructor() {
-        assertEquals(this.da.getCapacity(), 10);
+        this.da = new DynamicArray(2);
+        assertEquals(this.da.getCapacity(), 2);
+        assertEquals(this.da.getSize(), 0);
+    }
+
+    @Test
+    public void testAppend() {
+        this.da = new DynamicArray(2);
+        assertEquals(this.da.getCapacity(), 2);
+        assertEquals(this.da.getSize(), 0);
+
+        this.da.append(22);
+        assertEquals(this.da.getCapacity(), 2);
+        assertEquals(this.da.getSize(), 1);
+        assertEquals(this.da.getItem(0), 22);
+
+        this.da.append(33);
+        assertEquals(this.da.getCapacity(), 2);
+        assertEquals(this.da.getSize(), 2);
+        assertEquals(this.da.getItem(1), 33);
+
+        this.da.append(44);
+        assertEquals(this.da.getCapacity(), 4);
+        assertEquals(this.da.getSize(), 3);
+        assertEquals(this.da.getItem(2), 44);
     }
 }
